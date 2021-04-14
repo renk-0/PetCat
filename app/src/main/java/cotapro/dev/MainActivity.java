0cc18f98a7bdc353f44969ad236b2a6eb8fc6a81
Mkefs <Mauricio.291202@gmail.com>
2021-04-14 13:14:50 -0500

    Primer comit

---
+++ app/src/main/java/cotapro/dev/MainActivity.java
@@ -0,0 +1,14 @@
+package cotapro.dev;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		// Primer comit
+		setContentView(R.layout.activity_main);
+	}
+}
