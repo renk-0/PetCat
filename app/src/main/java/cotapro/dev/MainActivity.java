2fd7b95b243d7b001a7194423bc0bec77bd62b58
Cristopher Cerda Puga <66029017+Cristopher811@users.noreply.github.com>
2021-04-14 20:04:26 -0500

    Cristopher (#4)
    * grafitié el xml
    
    * Sensores iniciados
    
    Co-authored-by: Mkefs <68037616+Mkefs@users.noreply.github.com>

--- app/src/main/java/cotapro/dev/MainActivity.java
+++ app/src/main/java/cotapro/dev/MainActivity.java
@@ -2,13 +2,61 @@ package cotapro.dev;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		// Primer comit
-		setContentView(R.layout.activity_main);
-	}
+
+    SensorManager sensorManager;
+    Sensor sensor;
+    SensorEventListener sensorEventListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(sensor.TYPE_PROXIMITY);
+
+        if (sensor == null)
+            finish();
+        sensorEventListener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent event) {
+                if (event.values[0] < sensor.getMaximumRange()) {
+                    getWindow().getDecorView().setBackgroundColor(Color.BLACK);
+                }else {
+                    getWindow().getDecorView().setBackgroundColor(Color.WHITE);
+                }
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+            }
+        };
+        Start();
+    }
+    public void Start(){sensorManager.registerListener(sensorEventListener,sensor, 2000*1000);
+    }
+    public void Stop(){
+        sensorManager.unregisterListener(sensorEventListener);
+    }
+
+    @Override
+    protected void onPause() {
+        Stop();
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        Start();
+        super.onResume();
+    }
 }
