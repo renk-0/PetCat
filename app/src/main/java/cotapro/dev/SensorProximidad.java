1d26a99487eff91acd6168d8eec4da17e0a39998
Mkefs <Mauricio.291202@gmail.com>
2021-04-15 20:32:50 -0500

    Acelerometro colores

---
+++ app/src/main/java/cotapro/dev/SensorProximidad.java
@@ -0,0 +1,45 @@
+package cotapro.dev;
+
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SensorProximidad {
+	Sensor sensor;
+	SensorEventListener listener;
+
+	public SensorProximidad(SensorManager manager, AppCompatActivity activity) {
+		sensor = manager.getDefaultSensor(sensor.TYPE_PROXIMITY);
+		if (sensor == null) {
+			activity.finish();
+			throw new RuntimeException("Error al conseguir el sensor");
+		}
+		listener = new SensorEventListener() {
+			@Override
+			public void onSensorChanged(SensorEvent event) {
+				if (event.values[0] < sensor.getMaximumRange()) {
+					activity.getWindow().getDecorView().setBackgroundColor(Color.BLACK);
+				} else {
+					activity.getWindow().getDecorView().setBackgroundColor(Color.WHITE);
+				}
+			}
+			@Override
+			public void onAccuracyChanged(Sensor sensor, int accuracy) {}
+		};
+	}
+
+	public void start(SensorManager manager) {
+		if(!manager.registerListener(listener, sensor, SensorManager.SENSOR_DELAY_NORMAL)) {
+			Log.d("Mal", "Muy mal");
+		}
+	}
+
+	public void stop(SensorManager manager) {
+		manager.unregisterListener(listener);
+	}
+}
