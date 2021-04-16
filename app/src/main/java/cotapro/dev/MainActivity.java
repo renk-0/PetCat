5a75b49d1436a88da75cabbd86209b14f5674ce0
Mkefs <68037616+Mkefs@users.noreply.github.com>
2021-04-15 20:41:10 -0500

    Acelerometro colores (#5)

--- app/src/main/java/cotapro/dev/MainActivity.java
+++ app/src/main/java/cotapro/dev/MainActivity.java
@@ -2,61 +2,36 @@ package cotapro.dev;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.Color;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
     SensorManager sensorManager;
-    Sensor sensor;
-    SensorEventListener sensorEventListener;
+    SensorProximidad proximidad;
+    SensorAcelerometro acelerometro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        sensor = sensorManager.getDefaultSensor(sensor.TYPE_PROXIMITY);
-
-        if (sensor == null)
-            finish();
-        sensorEventListener = new SensorEventListener() {
-            @Override
-            public void onSensorChanged(SensorEvent event) {
-                if (event.values[0] < sensor.getMaximumRange()) {
-                    getWindow().getDecorView().setBackgroundColor(Color.BLACK);
-                }else {
-                    getWindow().getDecorView().setBackgroundColor(Color.WHITE);
-                }
-            }
-
-            @Override
-            public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
-            }
-        };
-        Start();
-    }
-    public void Start(){sensorManager.registerListener(sensorEventListener,sensor, 2000*1000);
-    }
-    public void Stop(){
-        sensorManager.unregisterListener(sensorEventListener);
+        proximidad = new SensorProximidad(sensorManager, this);
+        acelerometro = new SensorAcelerometro(sensorManager, this);
+        proximidad.start(sensorManager);
+        acelerometro.start(sensorManager);
     }
 
     @Override
     protected void onPause() {
-        Stop();
+        proximidad.stop(sensorManager);
+        acelerometro.stop(sensorManager);
         super.onPause();
     }
 
     @Override
     protected void onResume() {
-        Start();
+        proximidad.start(sensorManager);
+        acelerometro.start(sensorManager);
         super.onResume();
     }
 }
---
+++ app/src/main/java/cotapro/dev/SensorAcelerometro.java
@@ -0,0 +1,52 @@
+package cotapro.dev;
+
+import android.annotation.SuppressLint;
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Locale;
+
+public class SensorAcelerometro {
+	Sensor sensor;
+	SensorEventListener listener;
+	float maxVel =  28.5f;
+
+	public SensorAcelerometro(SensorManager manager, AppCompatActivity activity) {
+		sensor = manager.getDefaultSensor(sensor.TYPE_ACCELEROMETER);
+		if(sensor == null) {
+			activity.finish();
+			throw new RuntimeException("Error al conseguir el sensor de acelerometro");
+		}
+		listener = new SensorEventListener() {
+			@Override
+			public void onSensorChanged(SensorEvent event) {
+				float factor = event.values[0] +
+						event.values[1] +
+						event.values[2];
+				if(factor > maxVel) {
+					activity.getWindow().getDecorView().setBackgroundColor(Color.RED);
+				} else {
+					activity.getWindow().getDecorView().setBackgroundColor(Color.WHITE);
+				}
+			}
+
+			@Override
+			public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+			}
+		};
+	}
+	public void start(SensorManager manager) {
+		manager.registerListener(listener, sensor, 2000*1000);
+	}
+
+	public void stop(SensorManager manager) {
+		manager.unregisterListener(listener);
+	}
+}
