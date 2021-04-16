d714c9bafbfb357345e6969788dee9e0f6a9ac28
Carl-Ang <cadaanca@gmail.com>
2021-04-16 17:16:19 -0500

    Puse sensor Touch en MainActivity, no modifique nada mas

--- app/src/main/java/cotapro/dev/MainActivity.java
+++ app/src/main/java/cotapro/dev/MainActivity.java
@@ -2,13 +2,17 @@ package cotapro.dev;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     SensorManager sensorManager;
     SensorProximidad proximidad;
     SensorAcelerometro acelerometro;
+    private boolean isTouch = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +30,25 @@ public class MainActivity extends AppCompatActivity {
         proximidad.stop(sensorManager);
         acelerometro.stop(sensorManager);
         super.onPause();
+    }
+
+    public boolean onTouchEvent(MotionEvent event){
+        int X = (int) event.getX();
+        int Y = (int) event.getY();
+        int eventaction = event.getAction();
+
+        switch (eventaction) {
+            case MotionEvent.ACTION_DOWN:
+                this.getWindow().getDecorView().setBackgroundColor(Color.GREEN);
+                isTouch = true;
+                break;
+
+            case MotionEvent.ACTION_UP:
+                this.getWindow().getDecorView().setBackgroundColor(Color.BLUE);
+                break;
+        }
+        return true;
+
     }
 
     @Override
