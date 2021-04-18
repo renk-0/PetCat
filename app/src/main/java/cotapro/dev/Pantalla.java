0ab479c902b0573b5d17b938be222f4cb26c1f17
Mkefs <Mauricio.291202@gmail.com>
2021-04-17 23:02:30 -0500

    xml

--- app/src/main/java/cotapro/dev/Pantalla.java
+++ app/src/main/java/cotapro/dev/Pantalla.java
@@ -2,13 +2,18 @@ package cotapro.dev;
 
 import android.graphics.Color;
 
+import pl.droidsonroids.gif.GifImageView;
+
 public class Pantalla implements Runnable {
 	MainActivity activity;
+	GifImageView gato;
+	boolean transicionando = false;
 	int current_color;
 
 	public Pantalla(MainActivity activity) {
 		this.activity = activity;
 		current_color = Color.WHITE;
+		gato = activity.findViewById(R.id.gato);
 	}
 
 	@Override
